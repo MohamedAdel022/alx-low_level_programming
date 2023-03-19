a59ffd7bbfa3586f9edd3f9913a142f52e34f631
mohamed adel <madel7586@gmail.com>
2023-03-18 21:54:19 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function(start of the programe
+ *
+ * Return: 0 if succeed
+ */
+
+int main(void) 
+{
+  	printf("with proper grammar, but the outcome is a piece of art,");
+  	return 0;
+}
