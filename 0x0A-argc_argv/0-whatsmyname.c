df2abe3a6d79d2a3cd042355142eae01490b3444
Mohamed Adel <madel7586@gmail.com>
2023-07-05 14:30:48 +0300

    done

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,9 +6,9 @@
 *@argv: array of arguments
 *Return: 0
 */
-int main(int __attribute__((__unused__)) argc, char *argv[])
-{
-	printf("%s\n", argv[0]);
+int main(int argc, char *argv[]) {
+    // Print the program name (including the path)
+    printf("%s\n", argv[0]);
 
-	return (0);
+    return 0;
 }
